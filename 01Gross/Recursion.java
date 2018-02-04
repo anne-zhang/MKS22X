e41935f5a791cdf7e11a277ee13f1ebb48720f38
Anne Zhang <azhang14@stuy.edu>
2018-02-04 17:07:35 -0500

    fib :(

--- 01Gross/Recursion.java
+++ 01Gross/Recursion.java
@@ -5,6 +5,16 @@ public class Recursion{
 		}
 		return n * fact(n-1);
 	}
+	 public static int fib(int n){
+		return fibHelp(0, n, 0, 1);
+    }
+
+    public static int fibHelp(int count, int n, int prevsum, int sum){
+		if(count == n){
+	    	return prevsum;
+		}
+		return fibHelp(count + 1, n, sum, prevsum + sum);
+	}
 	
 	public static void main(String[] args){
 		System.out.println(fact(5));
@@ -12,6 +22,11 @@ public class Recursion{
 		System.out.println(fact(0));
 		System.out.println(fact(11));
 		System.out.println(fact(19)); //eek20istoobig
+		System.out.println(fib(0));
+		System.out.println(fib(1));
+		System.out.println(fib(2));
+		System.out.println(fib(3));
+		System.out.println(fib(10));
 		
 }
 }

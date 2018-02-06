0a8894997629683f743292d20564563624caa4ce
Anne Zhang <azhang14@stuy.edu>
2018-02-06 01:14:46 -0500

    figured out sqrt and also added exception when number is negative

--- 01Gross/Recursion.java
+++ 01Gross/Recursion.java
@@ -6,6 +6,9 @@ public class Recursion{
 		return n * fact(n-1);
 	}
 	 public static int fib(int n){
+	 	if (n < 0){
+	 		throw new IllegalArgumentException();
+	 	}
 		return fibHelp(0, n, 0, 1);
     }
 
@@ -15,8 +18,26 @@ public class Recursion{
 		}
 		return fibHelp(count + 1, n, sum, prevsum + sum);
 	}
-	
-	public static void main(String[] args){
+	public static double sqrt(double n){
+    	if(n < 0){
+      		throw new IllegalArgumentException();
+    	}
+    	return sqrtHelp(n, 1.0);
+  	}
+
+  	public static double sqrtHelp(double n, double guess){
+    	if(n == 0){
+      		return 0;
+    	}
+    	if(n == 1){
+      		return 1;
+    	}
+    	if(100 * Math.abs((guess * guess - n)/n) <= .00001){
+      		return guess;
+    	}
+    	return sqrtHelp(n, (n/guess + guess)/2);
+  	}
+	/*public static void main(String[] args){
 		System.out.println(fact(5));
 		System.out.println(fact(6));
 		System.out.println(fact(0));
@@ -24,9 +45,14 @@ public class Recursion{
 		System.out.println(fact(19)); //eek20istoobig
 		System.out.println(fib(0));
 		System.out.println(fib(1));
-		System.out.println(fib(2));
+		System.out.println(fib(-2));
 		System.out.println(fib(3));
 		System.out.println(fib(10));
+		System.out.println(sqrt(100));
+		System.out.println(sqrt(-1));
+		System.out.println(sqrt(0));
+		System.out.println(sqrt(1));
 		
-}
+		
+}*/
 }

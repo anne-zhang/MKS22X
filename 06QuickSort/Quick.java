e50ae45742a4dec379425888ff8503593bdb0d07
Anne Zhang <azhang14@stuy.edu>
2018-04-09 01:17:27 -0400

    changed partition back

--- 06QuickSort/Quick.java
+++ 06QuickSort/Quick.java
@@ -1,25 +1,32 @@
 import java.util.*;
 public class Quick{
     public static int[] partition(int[] data, int start, int end){
-		int p = (int) (Math.random()*(end-start) + start);
-        int pivot = data[p];
-        int low = start;
-        int high = end;
-        int i = start;
-        while (i <= high) {
-            if (data[i] == pivot) {
-                i++;
-            } else if (data[i] < pivot) {
-                swap(data, i, low);
-                low++;
-                i++;
-            } else {
-                swap(data, i, high);
-                high--;
-            }
-        }
-        int[] ary = {low, high};
-        return ary;
+	
+      
+	int random = (int)(Math.random() * (end - start -1 ) + start + 1);
+
+	int pivot = data[random];
+
+	data = swap(data, start, random);
+	int i = start + 1;
+        int lt = start;
+	int gt = end;
+	while(i <= gt){
+	    if(data[i] == pivot) i++;
+	    else if(data[i] < pivot){
+		data = swap(data, i, lt);
+		lt++;
+		i++;
+	    }
+	    else{
+		data = swap(data, i, gt);
+		gt--;
+	    }
+	}
+	int[] val = {lt, gt};
+	return val;
+	
+	
     }
    private static int[] swap(int[] a, int i, int j){
 	int hold = a[i];

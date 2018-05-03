16832714f06e491cde83df91aac6444b77cc3342
Anne Zhang <azhang14@stuy.edu>
2018-05-03 00:32:39 -0400

    silver?

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -72,4 +72,64 @@ public class USACO {
 	
     	
     	}
+    	
+    public static int silver(String filename){
+		int[][] current,old;
+		char[][] map;
+		try{
+	    	File f = new File(filename);
+	    	Scanner scan = new Scanner(f);
+
+	    int row = scan.nextInt();
+	    int col = scan.nextInt();
+	    int moves = scan.nextInt();
+
+	    map = new char[row][col];
+	    current = new int[row][col];
+	    old = new int[row][col];
+
+	    for(int i = 0; i < row; i++){
+			String line = scan.next();
+			for(int j = 0; j < col; j++){
+			    map[i][j] = line.charAt(j);
+			}
+	    }
+
+	    old[scan.nextInt()-1][scan.nextInt()-1] = 1;
+
+	    
+	    for (int i = 0; i < moves; i++){
+		for (int r = 0; r < row; r++){
+		    for (int c = 0; c < col; c++){
+			if (map[r][c] != '*'){
+			    int ans = 0;
+			    if(r+1 >= 0 && c >= 0 && r + 1 < row && c < col){
+				ans += old[r+1][c];
+			    }
+			    if(r - 1 >= 0 && c >= 0 && r - 1  < row && c < col){
+				ans += old[r-1][c];
+			    }
+			    if(r >= 0 && c + 1 >= 0 && r < row && c + 1 < col){
+				ans += old[r][c+1];
+			    }
+			    if(r >= 0 && c - 1 >= 0 && r < row && c - 1 < col){
+				ans += old[r][c-1];
+			    }
+			    current[r][c] = ans;
+			}
+		    }
+		}
+	        old = current;
+		current = new int[row][col];
+	    }
+      
+	    return old[scan.nextInt()-1][scan.nextInt()-1];
+	}
+	catch(FileNotFoundException e){
+	    System.out.println("File not found: " + filename);
+	    System.exit(1);
+	}
+	    
+	return 0;
+    }
 }

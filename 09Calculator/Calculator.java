e378c6fa4ad4689ee402e4e1297b4eb1c6138b42
Anne Zhang <azhang14@stuy.edu>
2018-06-03 23:28:05 -0400

    recoded with a little help

--- 09Calculator/Calculator.java
+++ 09Calculator/Calculator.java
@@ -1,38 +1,47 @@
 import java.util.*;
-public class Calculator{
+import java.io.*;
 
-	public static double eval(String n){
-		LinkedList<Double> Stack = new LinkedList<>();
-		String[] splitN = n.split(" ");
-		
-		for (int i = 0; i < splitN.length; i++){
-			
-				if(splitN[i].equals("+")){
-					double numOne = Stack.pop();
-					double numTwo = Stack.pop();
-					Stack.push(numOne + numTwo);
-				}
-				else if(splitN[i].equals("-")){
-					double numOne = Stack.pop();
-					double numTwo = Stack.pop();
-					Stack.push(numTwo - numOne);
-				}
-				else if(splitN[i].equals("*")){
-					double numOne = Stack.pop();
-					double numTwo = Stack.pop();
-					Stack.push(numOne * numTwo);
-				}
-				else if(splitN[i].equals("/")){
-					Stack.push(Stack.pop() / i);
-				}
-				else if(splitN[i].equals("%")){
-					Stack.push(Stack.pop() % i);
-				}
-				else{
-					Stack.push(Double.parseDouble(splitN[i]));
-				}
-			}
-			return Stack.pop();
-		}	
+public class Calculator{
+    
+    public static double eval(String s){
+	String[] list = s.split("\\s+");
+	@SuppressWarnings("unchecked")
+	LinkedList<String> stack = new LinkedList();
+	for (int x = 0; x < list.length; x++){
+	    try{
+		double temp = Double.parseDouble(list[x]);
+		stack.push(temp+"");
+	    }
+	    catch (NumberFormatException e){
+		double num2= Double.parseDouble(stack.pop());
+		double num1= Double.parseDouble(stack.pop());
+		if (list[x].equals("*")){
+		    stack.push(num1* num2+ "");
+		}
+		if (list[x].equals("/")){
+		    stack.push(num1/ num2+ "");
+		}
+		if (list[x].equals("%")){
+		    stack.push(num1% num2+ "");
+		}
+		if (list[x].equals("+")){
+		    stack.push(num1+ num2+ "");
+		}
+		if (list[x].equals("-")){
+		    stack.push(num1- num2+ "");
+		}
+	    }
+	}
+	return Double.parseDouble(stack.pop());
+    }
+        public static void main(String[] args){
+	System.out.println(eval("10 6 +"));
+	System.out.println(eval("3 3 - 4 * 3 3 -"));
+	System.out.println(eval("2 3 * 4 + 1 1 -"));
+	System.out.println(eval("2 10 /"));
+	System.out.println(eval("5 10 %"));
+	System.out.println(eval("0"));
+    }
+
 
 }
